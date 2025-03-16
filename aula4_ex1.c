37ecf195e38c0a05183891c312fed5de1a1d653a
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-03-15 22:38:24 -0300

    teste

---
+++ aula4_ex1.c
@@ -0,0 +1,20 @@
+//Crie um programa em C que recebe um valor de IMC e determina se o valor está no intervalo normal [18.5, 24.9]. A resposta deve ser "Intervalo normal" ou "Fora do intervalo normal".
+
+#include <stdio.h>
+
+int main() {
+    float imc;
+
+    printf("Digite o valor do IMC: ");
+    scanf("%f", &imc);
+
+    if (imc < 18.5) {
+        printf("Abaixo do normal\n");
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        printf("Intervalo normal\n");
+    } else {
+        printf("Fora do intervalo normal\n");
+    }
+
+    return 0;
+}
