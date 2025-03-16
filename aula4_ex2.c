0fcac7083d7443196b7cca0ab655be022b0dee4e
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-03-16 17:20:50 -0300

    Exercicios de sala de aula

---
+++ aula4_ex2.c
@@ -0,0 +1,10 @@
+//Sem usar return no corpo do if e do else. Crie um programa em C que recebe uma nota em valor real
+//(exemplo: 5.3) e mostra o conceito que o aluno recebeu, de acordo com a tabela:
+
+//Intervalo de nota	Conceito
+//      [9,10]	      A
+//      [8,9[	      B
+//      [7,8[	      C
+//      [6,7[	      D
+//      [0,6[	      F
+
---
+++ aula4_ex3.c
@@ -0,0 +1,14 @@
+//Usando if/else interno crie um programa em C que recebe o peso e altura de um usuário e imprime
+//qual o diagnóstico de acordo com a tabela disponível em: https://aps.bvs.br/apps/calculadoras/?page=6
+
+// IMC = Peso / Altura * 2
+
+//Valores de referência
+
+//IMC	                              Diagnóstico
+//menor que    18,5	                  baixo peso
+//entre        18,5 e 24,9	          intervalo normal
+//entre        25 e 29,9	          sobrepeso
+//entre        30 e 34,9	          obesidade classe I
+//entre        35 e 39,9	          obesidade classe II
+//maior que    40	                  obesidade classe III
