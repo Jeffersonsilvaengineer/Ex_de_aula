2b44ba15af706232c52f364d4eebff64f2477adf
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-03-19 22:48:04 -0300

    Exercicio de sala de aula

--- aula6_ex4.c
+++ aula6_ex4.c
@@ -5,18 +5,16 @@
 
 int main() {
     float raio, diametro, circunferencia, area;
-    while (1) {
-        printf("Digite o valor do raio do círculo: ");
-        scanf("%f", &raio);
-        if (raio == -1) {
-            break;
-        }
-        diametro = 2 * raio;
-        circunferencia = 2 * 3.14159 * raio;
-        area = 3.14159 * raio * raio;
-        printf("Diâmetro: %.2f\n", diametro);
-        printf("Circunferência: %.2f\n", circunferencia);
-        printf("Área: %.2f\n", area);
-    }
+
+    printf("Digite o valor do raio do círculo: ");
+    scanf("%f", &raio);
+        
+    diametro = 2 * raio;
+    circunferencia = 2 * 3.14159 * raio;
+    area = 3.14159 * raio * raio;
+    printf("Diâmetro: %.2f\n", diametro);
+    printf("Circunferência: %.2f\n", circunferencia);
+    printf("Área: %.2f\n", area);
+    
     return 0;
 }
