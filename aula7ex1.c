648f8fb06c8a4e1fd87cd6593b6eb11b006d02dc
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-03-25 19:42:35 -0300

    Exercicios de sala de aula

--- aula7ex1.c
+++ aula7ex1.c
@@ -1,1 +1,16 @@
 //Crie um programa em C que recebe um número e, usando for, soma todos os impares de 1 até o número recebido.
+
+#include <stdio.h>
+
+int main() {
+    int fim = 10;
+    int soma = 0;
+
+    for(int contador = 1; contador <= fim; contador++) {
+        if (contador %2 == 1)
+            soma++;
+        //printf("%d ", contador);
+        printf("%d ", soma);
+}
+    return 0;
+}
