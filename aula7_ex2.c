392bed612277e3848379e07e37ec7c1dbf7cdbdb
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-03-25 20:32:35 -0300

    Exercicios de sala de aula

--- aula7_ex2.c
+++ aula7_ex2.c
@@ -5,38 +5,36 @@ int main(void) {
   char tamanho;
   
   printf("Digite o tamanho recebido, F para encerrar:");
-  scanf("%c", &tamanho);
+  //scanf("%c", &tamanho);
+  tamanho = getchar();
+  getchar();
 
   int quantidade_P = 0;
   int quantidade_M = 0;
   int quantidade_G = 0;
 
-  while( tamanho != 'F' ) {
+  while( tamanho != 'F' || tamanho == 'f' ) {
     switch (tamanho) {
       case 'P':
+      case 'p':
 	quantidade_P++;
 	break;
-      case 'M': 
+      case 'M':
+      case "m";
 	quantidade_M++;
 	break;
       case 'G':
+      case "g";
 	quantidade_G++;
 	break;
     }
 
     printf("Digite o tamanho recebido, F para encerrar:");
-    scanf("%c", &tamanho);
+    //scanf("%c", &tamanho);
+    tamanho = getchar();
+    getchar();
   }
 
   printf("P: %d M: %d G: %d", quantidade_P, quantidade_M, quantidade_G);
   return 0;
 }
-
-
-//char letra;
-
-printf("Digite uma letra: ");
-
-letra = getchar();
-
-printf("Digitado: %c", letra);
