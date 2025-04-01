646b2acf382a65bf3973f333dc97925ecbefd192
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-01 19:36:45 -0300

    Exercicios de sala de aula

---
+++ aula8_ex01.c
@@ -0,0 +1,2 @@
+// Usando do...while crie um programa em C, um jogo para adivinhar um número, o programa deve receber um número se o número for igual à 7 mostre a mensagem "Você ganhou!" . 
+//Se o jogador errar o número mostre a mensagem Número errado. Tentativas restantes: N. (N é a quantidade restante) Se errar 3 vezes mostre Fim de jogo! e termine o programa.
