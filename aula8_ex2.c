9275490fe0d84d1b791baf77f7fe1d12cfe53104
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-01 19:40:12 -0300

    Exercicios de sala de aula

---
+++ aula8_ex2.c
@@ -0,0 +1,3 @@
+//Crie o jogo "É par?" em C. O programa recebe um número de 1 a 6 que é o valor de um dado jogado pelo usuário até que o número seja ímpar. 
+//Quando o número digitado é ímpar use o break para terminar, o jogo termina mostrando a soma total dos número pares digitados até o momento.
+
