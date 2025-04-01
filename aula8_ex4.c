5a3e3b45b5eeed2507f66118ba22a451a9f41ca3
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-01 20:30:08 -0300

    Exercicios de sala de aula

--- aula8_ex4.c
+++ aula8_ex4.c
@@ -1,1 +1,7 @@
+//Crie um programa em C que imprime a tabela verdade E, use repetição e variáveis A e B.
+//cada linha deve imprimir assim: A: 0 B: 0   0 && 0 = 0
 
+#include <stdio.h>
+int main(void){
+  for(int a = 0; a < 2; a++){
+    
