d8b20c448d6267b55134b34377b6835e04606dd5
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-01 20:08:09 -0300

    Exercicios de sala de aula

--- aula8_ex3.c
+++ aula8_ex3.c
@@ -1,1 +1,19 @@
+//Altere o programa anterior para mostrar todos os números de 1 a 20, exceto os múltiplos de 3. 
+//Use o comando continue para pular a impressão dos múltiplos de 3. A saída será: 1 2 4 5 7 8 10 11 13 14 16 17 19 20
 
+#include <stdio.h>
+
+int main(void) {
+  int x = 0;
+
+  while( x < 20 ) {
+    x++;      
+    if( x % 3 == 0) {
+      continue;
+    }
+    printf("%d ", x);
+  }
+
+  printf("\nValor atual de x: %d", x);
+  return 0;
+}
