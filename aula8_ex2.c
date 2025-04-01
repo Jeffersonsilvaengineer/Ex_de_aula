7e78890115002a244b8fd8985756b23c393981cc
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-01 19:51:16 -0300

    Exercicios de sala de aula

--- aula8_ex2.c
+++ aula8_ex2.c
@@ -1,3 +1,12 @@
 //Crie o jogo "É par?" em C. O programa recebe um número de 1 a 6 que é o valor de um dado jogado pelo usuário até que o número seja ímpar. 
 //Quando o número digitado é ímpar use o break para terminar, o jogo termina mostrando a soma total dos número pares digitados até o momento.
 
+#include <stdio.h>
+
+int main(void) {
+  int x = 0;
+  int dado = 0;
+do{
+   printf("Jogue o dado: ")
+   scanf("%d", &dado);
+   if dado %
