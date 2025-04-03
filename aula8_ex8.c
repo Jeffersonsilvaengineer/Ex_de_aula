c2d1ad2ced3397bff5a8241d959f38bd192c83ed
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-02 21:27:19 -0300

    Exercicios de sala de aula

---
+++ aula8_ex8.c
@@ -0,0 +1,5 @@
+// Crie uma função chamada testa_par que recebe um número inteiro e retorna 1 se o número for par e 0 se for ímpar. 
+// Na função main, teste a função com alguns valores e exiba os resultados. Exemplo de saída:
+
+// 4 é par? 1
+//5 é par? 0
