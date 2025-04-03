436f521ae5f286eba09a8c7fc144464afbc4d62a
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-02 21:25:44 -0300

    Create aula8_ex7.c

---
+++ aula8_ex7.c
@@ -0,0 +1,7 @@
+//Crie um programa em C que recebe 4 números inteiros de 0 a 10 e imprime uma linha de asteriscos para cada número recebido.
+// exemplo: 6 7 3 9
+
+//******
+//*******
+//***
+//*********
