3f59d5ad9a145075288bae4c9d5b13366fc72aef
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-09 21:48:00 -0300

    Exercicios de sala de aula

--- aula9_ex1.c
+++ aula9_ex1.c
@@ -18,3 +18,23 @@ int main(void) {
 int multi3(int y) {
   return 3 * y;
 } */
+
+
+#include <stdio.h>
+
+void multi3(int *y);
+
+int main(void) {
+  int x = 5;
+  printf("X antes: %d\n", x);
+
+  multi3(&x);
+  printf("Resultado: %d\n", x);
+
+  printf("X depois: %d\n", x);
+  return 0;
+}
+
+void multi3(int *y) {
+  *y = *y *3;
+}
