067c95f51960acf2cd7cb67112446688e9695ce2
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-09 21:25:01 -0300

    Exercicios de sala de aula

---
+++ aula9_ex3.c
@@ -0,0 +1,6 @@
+//Use passagem por referência para alterar o programa anterior de modo que subtrai3() receba o endereço de numero.
+
+//digite um numero: 7
+//recebido: 7
+//resultado: 4
+//atual: 4
