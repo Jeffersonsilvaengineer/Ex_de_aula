2922af24056ccedb2bab6fdf81d17d559e966086
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-29 20:08:57 -0300

    Exercicios de sala de aula

---
+++ aula11_ex1.c
@@ -0,0 +1,4 @@
+//Crie um programa em C que declare um vetor de 5 números inteiros, inicialize-o com valores de sua escolha e imprima cada elemento do vetor junto com seu índice.
+//seguindo o exemplo:
+// x[0] = 2;
+// x[1] = 3;
