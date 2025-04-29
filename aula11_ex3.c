d805d8cec10872fc451a0b9518d4134c43c020f4
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-29 20:10:49 -0300

    Exercicios de sala de aula

---
+++ aula11_ex3.c
@@ -0,0 +1,3 @@
+//Crie um programa em C que recebe 4 notas, armazene as notas em um vetor. Some o total das notas usando for, guarde na variável total. 
+//Mostre o resultado com duas casas decimais.
+
