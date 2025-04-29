ff2f816e247cd6bd0f4bc9f3b1af406d5e44d5ba
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-29 19:53:32 -0300

    Exercicios de sala de aula

---
+++ aula12_ex2.c
@@ -0,0 +1,3 @@
+// Se listarmos todos os números naturais menores que 10 múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9. A soma desses múltiplos é 23. 
+//Crie um programa em C que encontre a soma de todos os múltiplos de 3 ou 5 abaixo de um número digitado pelo usuário
+
