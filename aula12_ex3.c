472952aaaa4cca99a6c8061f3f7c727658eed789
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-29 19:59:01 -0300

    Exercicios de sala de aula

---
+++ aula12_ex3.c
@@ -0,0 +1,4 @@
+//Crie um programa em C que recebe uma senha de um usuário e se a senha for igual à palavra "azimo" 
+//mostre a mensagem: "Seja bem vindo!" e se o usuário errar a senha, mostre a mensagem: "Senha errada. 
+//Tentativas restantes: 2", se o usuário errar 3 vezes mostra a mensagem: "Tente novamente mais tarde".
+
