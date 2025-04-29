ff73708c54ed233a62faa2ac5aba501757ca986e
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-29 20:09:53 -0300

    Exercicios de sala de aula

---
+++ aula11_ex2.c
@@ -0,0 +1,2 @@
+//Crie um programa que use uma constante PI com o valor 3.14159. 
+//Calcule a área de um círculo com raio digitado pelo usuário usando a fórmula área = PI * raio * raio e imprima o resultado.
