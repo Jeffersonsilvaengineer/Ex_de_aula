05d765a3405cb615ad24589e0cac644523c582e1
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-30 21:41:19 -0300

    Exercicios de sala de aula

---
+++ aula10_ex10.c
@@ -0,0 +1,1 @@
+//Crie um programa que importe o arquivo util.h. O programa recebe o valor do raio digitado pelo usuário e mostra os valores de diâmetro, circunferência e raio usando as funções de util.h.
