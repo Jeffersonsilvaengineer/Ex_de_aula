0867512dbb246a5a4fb0861cbc4c3612f086d571
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-04-30 21:18:01 -0300

    Exercicios de sala de aula

---
+++ aula12_ex5.c
@@ -0,0 +1,3 @@
+//Crie um programa que recebe uma senha e determina se ela possui 8 ou mais caracteres. 
+//Se for menor que 8 mostre a mensagem: "Erro na senha". Se for maior ou igual a 8 imprima: "Senha conforme".
+
