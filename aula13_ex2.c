c112b20a5956dcbd01c9e7860353600beb81d669
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-05-07 23:48:29 -0300

    Exercicios de sala de aula

--- aula13_ex2.c
+++ aula13_ex2.c
@@ -1,2 +1,43 @@
 //Ajuste o programa do ordenamento para colocar em ordem crescente todos os elementos do vetor.
 
+#include <stdio.h>
+
+void ordenar(float numeros[], int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (numeros[j] > numeros[j + 1]) {
+                float temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void exibirVetor(float numeros[], int tamanho) {
+    printf("Vetor: ");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%.2f ", numeros[i]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    float numeros[5];
+    int tamanho = 5;
+
+    for (int i = 0; i < tamanho; i++) {
+        printf("Digite o número %d: ", i + 1);
+        scanf("%f", &numeros[i]);
+    }
+
+    printf("Vetor original:\n");
+    exibirVetor(numeros, tamanho);
+
+    ordenar(numeros, tamanho);
+
+    printf("Vetor ordenado em ordem crescente:\n");
+    exibirVetor(numeros, tamanho);
+
+    return 0;
+}
--- aula13_ex3.c
+++ aula13_ex3.c
@@ -1,1 +1,46 @@
 //Crie um programa em C que recebe duas strings e verifique se são anagramas. Use strlen e ordenação de vetores.
+
+#include <stdio.h>
+#include <string.h>
+
+void ordenarString(char str[]) {
+    int tamanho = strlen(str);
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                char temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int saoAnagramas(char str1[], char str2[]) {
+    if (strlen(str1) != strlen(str2)) {
+        return 0;
+    }
+
+    ordenarString(str1);
+    ordenarString(str2);
+
+    return strcmp(str1, str2) == 0;
+}
+
+int main(void) {
+    char str1[100], str2[100];
+
+    printf("Digite a primeira string: ");
+    scanf("%s", str1);
+
+    printf("Digite a segunda string: ");
+    scanf("%s", str2);
+
+    if (saoAnagramas(str1, str2)) {
+        printf("As strings são anagramas.\n");
+    } else {
+        printf("As strings não são anagramas.\n");
+    }
+
+    return 0;
+}
