92f5bc815ed163762ae3aa9d92bd884e13b81138
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-05-13 19:52:11 -0300

    Exercicios de sala de aula

--- aula13_ex4.c
+++ aula13_ex4.c
@@ -5,3 +5,24 @@
 //**
 //***
 
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        for (int i = 1; i <= numero; i++) {
+            for (int j = 1; j <= i; j++) {
+                printf("*");
+            }
+            printf("\n");
+        }
+    } else {
+        printf("Por favor, digite um numero positivo.\n");
+    }
+
+    return 0;
+}
