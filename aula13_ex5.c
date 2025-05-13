e4c3a977307e4669c5c568f7b62a0e17478bf58a
Jefferson Souza <jeffersonsilvaengineer@gmail.com>
2025-05-13 19:46:15 -0300

    Exercicios de sala de aula

---
+++ aula13_ex5.c
@@ -0,0 +1,15 @@
+//Ajuste o programa anterior para receber após a impressão uma letra: A para desenhar o triângulo à esquerda e D para desenhar o triângulo à direita. F encerra o programa.
+
+//Digite um numero: 3
+//*
+//**
+//***
+//D
+//  *
+// **
+//***
+//A
+//*
+//**
+//***
+//F
